6ea29c09425ad8ff8d77718c6b2d98a62dd6a2da
Lim Pei En <limpeien123@gmail.com>
2024-03-30 12:45:27 +0800

    Create new EventListPanel class

---
+++ src/main/java/seedu/address/ui/EventListPanel.java
@@ -0,0 +1,49 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.model.tag.EventTag;
+
+/**
+ * Panel containing the list of events.
+ */
+public class EventListPanel extends UiPart<Region> {
+    private static final String FXML = "EventListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(EventListPanel.class);
+
+    @FXML
+    private ListView<EventTag> eventListView;
+
+    /**
+     * Creates a {@code EventListPanel} with the given {@code ObservableList}.
+     */
+    public EventListPanel(ObservableList<EventTag> eventList) {
+        super(FXML);
+        eventListView.setItems(eventList);
+        eventListView.setCellFactory(listView -> new EventListViewCell());
+    }
+
+    /**
+     * Custom {@code ListCell} that displays the graphics of a {@code Event} using a {@code EventCard}.
+     */
+    class EventListViewCell extends ListCell<EventTag> {
+        @Override
+        protected void updateItem(EventTag event, boolean empty) {
+            super.updateItem(event, empty);
+
+            if (empty || event == null) {
+                setGraphic(null);
+                setText(null);
+            } else {
+                setGraphic(new EventCard(event, getIndex() + 1).getRoot());
+            }
+        }
+    }
+
+}
